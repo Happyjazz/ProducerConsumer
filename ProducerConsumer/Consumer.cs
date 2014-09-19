6d86493645797edb3b3b8041093c49747ef83ba3
Martin <martin@happyjazz.eu>
2014-09-19 12:31:10 +0200

    Færdig og fungerer!
    Husk at lave Monitor.PulseAll(), når du har Monitor.Wait()

---
+++ ProducerConsumer/Consumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Consumer
+    {
+        private int _max;
+        private BoundedBuffer _buffer;
+        public Consumer(BoundedBuffer buffer, int ExpectedAmount)
+        {
+            _buffer = buffer;
+            _max = ExpectedAmount;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < _max; i++)
+            {
+                int temp = _buffer.Take();
+                
+            }
+        }
+    }
+}
---
+++ ProducerConsumer/Producer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Producer
+    {
+        private BoundedBuffer _buffer;
+        private int _max;
+
+        public Producer(BoundedBuffer buffer, int howManyToProduce)
+        {
+            _buffer = buffer;
+            _max = howManyToProduce;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < _max; i++)
+            {
+                _buffer.Put(i);
+            }
+        }
+    }
+}
